af88e446fcd5adb3094a9b0630ee72fa8f01dbda
Manuel Ramírez <l21010310@zamora.tecnm.mx>
2025-04-02 09:43:30 -0600

    Cambios BD 2 de Abril
    Falta la conexion funcionando...

--- PedidosREST/main.py
+++ PedidosREST/main.py
@@ -1,6 +1,10 @@
 #Importar la clase fastapi del framework#
+from email.policy import default
+
 import uvicorn
 from fastapi import FastAPI
+
+from dao.database import Conexion
 from routers import pedidosRouter,productosRouter, usuariosRouter
 
 #Crear una instancia de la clase fastapi#
@@ -13,6 +17,18 @@ app.include_router(usuariosRouter.router)
 async def home():
     salida = {"mensaje": "Bienvenido a la PEDIDOSREST"}
     return salida
+
+@app.on_event("startup")
+async def startup():
+    print("Conectando con MongodDB")
+    conexion=Conexion()
+    app.conexion=conexion
+    app.db=conexion.getDB()
+
+@app.on_event("shutdown")
+async def shutdown():
+    print("Cerrando la conexion con MongoDB")
+    app.conexion.cerrar()
 
 if __name__ == '__main__':
     uvicorn.run("main:app", host='127.0.0.1', reload=True)
